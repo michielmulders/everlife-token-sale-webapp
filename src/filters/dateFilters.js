4c60e38f8c8ae5818665b31d3c123839a3022598
cls <shwethawh@gmail.com>
2018-07-24 20:05:59 +0530

    Comment dateFilters.js

--- src/filters/dateFilters.js
+++ src/filters/dateFilters.js
@@ -1,6 +1,11 @@
 import moment from 'moment'
 import Vue from 'vue'
 
+/*   outcome/
+ * This function will filter the valid value
+ * and return the date in format MM/DD/YYYY hh:mm.
+ *  
+ */
 Vue.filter('formatDate', function(value) {
   if (value) {
     return moment(String(value)).format('MM/DD/YYYY hh:mm')
